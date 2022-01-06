aa063b03505b22474556fe31e7891f90bf49844d
Anton Baht <anton@baht.dk>
2022-01-06 12:22:47 +0100

    Evaluate mins less often in blk algo

--- assignment1/matmult_blk.c
+++ assignment1/matmult_blk.c
@@ -1,12 +1,13 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 void
 matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs) {
-  int i1, i2, j, l1, l2;
+  int i1, i2, j, l1, l2, imin, lmin;
 
   /* initializing C to be zero */
   for ( i1 = 0; i1 < m; i1+=bs ) {
+    imin = MIN(m-i1, bs);
     for ( j = 0; j < n; j++ ) {
-        for ( i2 = 0; i2<MIN(m-i1, bs); i2++){
+        for ( i2 = 0; i2< imin; i2++){
             C[i1+i2][j] = 0;
         }
     }
@@ -14,14 +15,16 @@ matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs) {
 
   /* matrix multiplication */
   for ( i1 = 0; i1 < m; i1+=bs ) {
+    imin = MIN(m-i1, bs);
     for ( l1 = 0; l1 < k; l1+=bs) {
-        for ( j = 0; j < n; j++ ) {
-            for ( l2=0; l2<MIN(k-l1,bs); l2++){
-                for ( i2 = 0; i2<MIN(m-i1, bs); i2++){
-                    C[i1+i2][j] += A[i1+i2][l1+l2]*B[l1+l2][j];
-                }
-            }
-        }
+      lmin = MIN(k-l1, bs);
+      for ( j = 0; j < n; j++ ) {
+          for ( l2=0; l2 < lmin; l2++){
+              for ( i2 = 0; i2 < imin; i2++){
+                  C[i1+i2][j] += A[i1+i2][l1+l2]*B[l1+l2][j];
+              }
+          }
+      }
     }
   }
 }
